397d1995be8b7bc9718bb71d61d3d00ce22b04e6
Park Jun Seok <112086285+YBSeok@users.noreply.github.com>
2025-09-02 15:43:47 +0900

    feat: 바텀 시트 라이브러리 적용 (#9)
    * feat: add route pages
    
    * feat: add search, category, ranking page
    
    * feat: delete space between page & tab bar
    
    * feat: fix style in category page
    
    * feat: add bottom sheet

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-reanimated';
 
 export default function RootLayout() {
@@ -13,13 +14,13 @@ export default function RootLayout() {
   }
 
   return (
-    <>
+    <GestureHandlerRootView>
       <Stack>
         <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
         <Stack.Screen name='+not-found' />
         <Stack.Screen name='home/search' options={{ headerShown: false }} />
       </Stack>
       <StatusBar style='auto' />
-    </>
+    </GestureHandlerRootView>
   );
 }
