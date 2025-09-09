0cad0b7bb30807b24b4a8e1a62b9568f7842b806
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-09-10 02:20:30 +0900

    feat: 계정 찾기 ui

---
+++ app/auth/find/id/index.tsx
@@ -0,0 +1,57 @@
+import ArrowLeftIcon from '@/assets/icons/ic_arrow_left.svg';
+import { LongButton } from '@/components/common/buttons/LongButton';
+import DefaultModal from '@/components/common/modals/DefaultModal';
+import Navigation from '@/components/layout/Navigation';
+import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
+import { useEffect, useRef, useState } from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+export default function Index() {
+  const router = useRouter();
+  const { open } = useLocalSearchParams<{ open?: string }>();
+  const openedRef = useRef(false);
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    if (open === 'fail' && !openedRef.current) {
+      openedRef.current = true;
+      setTimeout(() => {
+        setVisible(true); // 혹은 bottomSheetRef.current?.present();
+      }, 200); // 0.1초 정도 지연
+    }
+  }, [open]);
+
+  return (
+    <SafeAreaView className='flex-1 bg-white'>
+      <Stack.Screen options={{ headerShown: false }} />
+      <Navigation
+        left={<ArrowLeftIcon width={20} height={20} />}
+        onLeftPress={() => router.back()}
+      />
+
+      <View className='p-5'>
+        <Text className='text-h-lg font-extrabold text-gray-700 mb-[12px]'>
+          계정 찾기
+        </Text>
+        <Text className='text-b-md font-bold text-gray-700 mb-[40px]'>
+          휴대폰 본인인증이 필요합니다.
+        </Text>
+        <LongButton
+          label='휴대폰 본인인증'
+          onPress={() => {
+            router.push('/auth/phone');
+          }}
+        />
+      </View>
+      <DefaultModal
+        visible={visible}
+        onConfirm={() => setVisible(false)}
+        onCancel={() => setVisible(false)}
+        title='계정을 찾을 수 없습니다.'
+        message='해당 정보로 가입된 계정을 찾을 수 없습니다.'
+        secondMessage='다시 한번 확인해주시거나, 회원가입을 진행해 주세요'
+        confirmText='회원가입'
+        cancelText='닫기'
+      />
+    </SafeAreaView>
+  );
+}
--- components/common/modals/DefaultModal.tsx
+++ components/common/modals/DefaultModal.tsx
@@ -10,6 +10,7 @@ type DefaultModalProps = {
   confirmText?: string;
   cancelText?: string;
   singleButton?: boolean;
+  secondMessage?: string;
 };
 
 export default function DefaultModal({
@@ -21,6 +22,7 @@ export default function DefaultModal({
   confirmText = '확인',
   cancelText = '취소',
   singleButton = false,
+  secondMessage,
 }: DefaultModalProps) {
   return (
     <Modal
@@ -30,37 +32,44 @@ export default function DefaultModal({
       onRequestClose={onCancel}
     >
       <View className='flex-1 bg-black/60 items-center justify-center px-4'>
-        <View className='bg-white rounded-xl items-center justify-center max-w-xs pt-7 pb-1'>
+        <View className='bg-white rounded-[12px] items-center justify-center max-w-xs pt-7 overflow-hidden'>
           {title && (
-            <Text className='font-bold text-[16px] text-gray-900 mb-2 text-center'>
+            <Text className='font-extrabold text-b-sm text-gray-900 mb-[15px] text-center'>
               {title}
             </Text>
           )}
-          <Text className='text-[14px] text-gray-700 text-center mb-5 leading-5'>
+          <Text className='text-c2 text-gray-500 font-bold text-center leading-5'>
             {message}
           </Text>
+          {secondMessage && (
+            <Text className='text-c2 text-gray-500 font-bold text-center'>
+              {secondMessage}
+            </Text>
+          )}
           <View
             className={
               singleButton
-                ? 'flex-row justify-center items-center w-full border-t border-gray-100 mt-0.5'
-                : 'flex-row items-center w-full border-t border-gray-100 mt-0.5'
+                ? 'flex-row justify-center items-center w-full border-t border-gray-100 mt-5'
+                : 'flex-row items-center w-full border-t border-gray-100 mt-[22px]'
             }
           >
             {!singleButton && (
               <TouchableOpacity
-                className='flex-1 py-3 items-center justify-center border-r border-gray-100'
+                className='flex-1  py-[15px] items-center justify-center border-r border-gray-100 active:bg-gray-100'
                 onPress={onCancel}
                 activeOpacity={0.7}
               >
-                <Text className='text-[16px] text-gray-900'>{cancelText}</Text>
+                <Text className='text-b-md text-gray-500 font-bold'>
+                  {cancelText}
+                </Text>
               </TouchableOpacity>
             )}
             <TouchableOpacity
-              className='flex-1 py-3 items-center justify-center'
+              className='flex-1 py-[15px] items-center justify-center active:bg-gray-100 overflow-hidden'
               onPress={onConfirm}
               activeOpacity={0.7}
             >
-              <Text className='text-[16px] text-green-600 font-medium'>
+              <Text className='text-b-md text-green-600 font-bold'>
                 {confirmText}
               </Text>
             </TouchableOpacity>
