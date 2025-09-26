9782e7a80af7ed101300372a26f24e8af0e86357
YBSeok <112086285+YBSeok@users.noreply.github.com>
2025-09-26 16:53:32 +0900

    feat: add mypage api

--- hooks/useMain.ts
+++ hooks/useMain.ts
@@ -1,6 +1,5 @@
 import { mainAPI } from '@/services/home';
 import { MainScreenInfo } from '@/types/models/main';
-import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { useState } from 'react';
 
@@ -34,11 +33,4 @@ export const useMain = () => {
     setIsLoading,
     mainScreenInfo,
   };
-};
-
-export const useFetchMainScreenQuery = () => {
-  return useQuery({
-    queryKey: ['mainScreen'],
-    queryFn: () => mainAPI.getMainScreen(),
-  });
 };
