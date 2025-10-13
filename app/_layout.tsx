28a8a15a757bbd7047b5fc5bb212d65363eb5c3f
YBSeok <112086285+YBSeok@users.noreply.github.com>
2025-10-14 00:57:32 +0900

    feat: add sentry for debugging in pulish

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,25 @@
-import 'react-native-reanimated';
-import 'react-native-url-polyfill/auto';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import * as Sentry from '@sentry/react-native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-reanimated';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import 'react-native-url-polyfill/auto';
 
-export default function RootLayout() {
+if (!__DEV__) {
+  Sentry.init({
+    dsn: 'https://94cf4844eeacc7628b50e1af33131335@o4510182859800577.ingest.us.sentry.io/4510182861701120',
+    sendDefaultPii: true,
+    enableLogs: true,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1,
+    integrations: [Sentry.mobileReplayIntegration()],
+  });
+}
+
+function RootLayout() {
   const queryClient = new QueryClient();
 
   const [loaded] = useFonts({
@@ -34,3 +45,5 @@ export default function RootLayout() {
     </QueryClientProvider>
   );
 }
+
+export default __DEV__ ? RootLayout : Sentry.wrap(RootLayout);
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,15 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
 const { getDefaultConfig } = require('expo/metro-config');
-const config = getDefaultConfig(__dirname);
-
+const { withSentryConfig } = require('@sentry/react-native/metro');
 const {
   wrapWithReanimatedMetroConfig,
 } = require('react-native-reanimated/metro-config');
 
+/** @type {import('expo/metro-config').MetroConfig} */
+
+// 2. Expo의 기본 설정을 가져옵니다.
+const config = getDefaultConfig(__dirname);
+
+// 3. SVG 및 기타 설정을 추가합니다.
 config.transformer.babelTransformerPath = require.resolve(
   'react-native-svg-transformer',
 );
@@ -14,4 +18,8 @@ config.resolver.assetExts = config.resolver.assetExts.filter(
 );
 config.resolver.sourceExts.push('svg');
 
-module.exports = wrapWithReanimatedMetroConfig(config);
+// 4. Reanimated 설정으로 한번 감싸고,
+const reanimatedConfig = wrapWithReanimatedMetroConfig(config);
+
+// 5. 마지막으로 Sentry 설정으로 전체를 감싸서 내보냅니다.
+module.exports = withSentryConfig(reanimatedConfig);
