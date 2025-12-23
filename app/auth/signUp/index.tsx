2bbfe2c9b15fc63921b030189c90e1078fabc7cd
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-12-23 19:28:45 +0900

    fix: 바텀시트 렌더링 구조 변경으로 안드로이드 터치 이슈 수정 (#120)

--- app/auth/signUp/index.tsx
+++ app/auth/signUp/index.tsx
@@ -17,7 +17,6 @@ import {
   isSamePassword,
 } from '@/utils/validation';
 import BottomSheet from '@gorhom/bottom-sheet';
-import { PortalProvider } from '@gorhom/portal';
 import { Stack, useRouter } from 'expo-router';
 import { useMemo, useRef, useState } from 'react';
 import { Pressable, Text, View } from 'react-native';
@@ -136,158 +135,156 @@ export default function Index() {
   return (
     <SafeAreaView className='flex-1 bg-white'>
       <Stack.Screen options={{ headerShown: false }} />
-      <PortalProvider>
-        <Navigation
-          left={<ArrowLeftIcon width={20} height={20} />}
-          onLeftPress={() => router.back()}
-        />
+      <Navigation
+        left={<ArrowLeftIcon width={20} height={20} />}
+        onLeftPress={() => router.back()}
+      />
 
-        <View className='p-5'>
-          <Text className='text-h-lg font-n-eb text-gray-700 mb-[12px]'>
-            회원가입
-          </Text>
-          <Text className='text-b-md font-n-bd text-gray-700 mb-[8px]'>
-            계정 생성 후,
-          </Text>
-          <Text className='text-b-md font-n-bd text-gray-700 mb-[25px]'>
-            휴대폰 본인인증을 완료하면 가입이 완료됩니다.
-          </Text>
+      <View className='p-5'>
+        <Text className='text-h-lg font-n-eb text-gray-700 mb-[12px]'>
+          회원가입
+        </Text>
+        <Text className='text-b-md font-n-bd text-gray-700 mb-[8px]'>
+          계정 생성 후,
+        </Text>
+        <Text className='text-b-md font-n-bd text-gray-700 mb-[25px]'>
+          휴대폰 본인인증을 완료하면 가입이 완료됩니다.
+        </Text>
 
-          <View className='space-y-[18px] mb-[25px]'>
-            <View>
-              <TextField
-                menu={1}
-                value={email}
-                onChangeText={handleChangeEmail}
-                // onEndEditing={handleEmailEndEditing}
-                placeholder='이메일을 입력해주세요.'
-                firstMessage={
-                  isExistsEmail ? '이미 가입된 이메일입니다.' : '이메일 형식'
-                }
-                validateFirst={(v) => isEmail(v) && !isExistsEmail}
-              />
-            </View>
-            <View>
-              <TextField
-                menu={1}
-                value={password}
-                onChangeText={setPassword}
-                placeholder='비밀번호를 입력해주세요.'
-                firstMessage='8-20자 이내'
-                secondMessage='영문, 숫자, 특수문자 포함'
-                validateFirst={isLen8to20}
-                validateSecond={hasPasswordComposition}
-                secureTextEntry={true}
-              />
-            </View>
-            <View>
-              <TextField
-                menu={1}
-                value={confirmPassword}
-                onChangeText={setConfirmPassword}
-                placeholder='비밀번호를 다시 입력해주세요.'
-                firstMessage='비밀번호 일치'
-                validateFirst={(t) => isSamePassword(password, t)}
-                secureTextEntry={true}
-              />
-            </View>
-          </View>
-
-          <LongButton
-            label={isPending ? '확인 중...' : '휴대폰 본인인증'}
-            onPress={onPressPhoneAuth}
-            disabled={!canSubmit || isPending}
-          />
-        </View>
-
-        <BottomSheetLayout snapPoints={snapPoints} sheetRef={sheetRef}>
-          {/* ===== 서비스 약관 동의 ===== */}
-          <View className='px-5 pt-[30px] pb-[45px]'>
-            <Pressable
-              onPress={toggleAll}
-              className='flex-row items-center mb-[28px] '
-              hitSlop={8}
-            >
-              {isAllChecked ? (
-                <CircleCheckGreenIcon className='mr-[11px]' />
-              ) : (
-                <CircleCheckGrayIcon className='mr-[11px]' />
-              )}
-
-              <Text className='text-b-md font-n-eb text-gray-700 items-center'>
-                서비스 이용 약관 동의(전체)
-              </Text>
-            </Pressable>
-
-            <View className='gap-y-[25px] mb-[25px]'>
-              {TERMS.map(({ id, terms, essential }, idx) => {
-                const checked = checkedSet.has(idx);
-                return (
-                  <View
-                    key={idx}
-                    className='flex-row items-center justify-between'
-                  >
-                    <Pressable
-                      onPress={() => toggleItem(idx)}
-                      className='flex-row items-center '
-                      hitSlop={8}
-                    >
-                      <Svg
-                        width={18}
-                        height={18}
-                        viewBox='0 0 18 18'
-                        fill='none'
-                        className='mr-[13px]'
-                      >
-                        <Path
-                          d='M16.5 4.5L6.5 15L1.5 9'
-                          stroke={checked ? '#50D88F' : '#C9CDD2'}
-                          strokeWidth={1.5}
-                          strokeLinecap='round'
-                          strokeLinejoin='round'
-                        />
-                      </Svg>
-                      {essential ? (
-                        <Text
-                          className={`text-b-sm font-n-bd text-gray-400 ${checked ? 'text-gray-700' : 'text-gray-400'}`}
-                        >
-                          [필수]
-                        </Text>
-                      ) : (
-                        <Text
-                          className={`text-b-sm font-n-bd text-gray-400 ${checked ? 'text-gray-700' : 'text-gray-400'}`}
-                        >
-                          [선택]
-                        </Text>
-                      )}
-                      <Text
-                        className={`ml-[2px] font-n-bd text-b-sm ${
-                          !checked && 'text-b-sm text-gray-400'
-                        }`}
-                      >
-                        {terms}
-                      </Text>
-                    </Pressable>
-                    <Pressable
-                      onPress={() => {
-                        setIsTermsModalVisible(true);
-                        setSelectedTerm(id);
-                      }}
-                    >
-                      <RightArrowIcon />
-                    </Pressable>
-                  </View>
-                );
-              })}
-            </View>
-            <LongButton
-              label='동의하고 계속하기'
-              onPress={onPressSubmit}
-              disabled={!isEssentialChecked}
+        <View className='space-y-[18px] mb-[25px]'>
+          <View>
+            <TextField
+              menu={1}
+              value={email}
+              onChangeText={handleChangeEmail}
+              // onEndEditing={handleEmailEndEditing}
+              placeholder='이메일을 입력해주세요.'
+              firstMessage={
+                isExistsEmail ? '이미 가입된 이메일입니다.' : '이메일 형식'
+              }
+              validateFirst={(v) => isEmail(v) && !isExistsEmail}
             />
           </View>
-        </BottomSheetLayout>
-      </PortalProvider>
+          <View>
+            <TextField
+              menu={1}
+              value={password}
+              onChangeText={setPassword}
+              placeholder='비밀번호를 입력해주세요.'
+              firstMessage='8-20자 이내'
+              secondMessage='영문, 숫자, 특수문자 포함'
+              validateFirst={isLen8to20}
+              validateSecond={hasPasswordComposition}
+              secureTextEntry={true}
+            />
+          </View>
+          <View>
+            <TextField
+              menu={1}
+              value={confirmPassword}
+              onChangeText={setConfirmPassword}
+              placeholder='비밀번호를 다시 입력해주세요.'
+              firstMessage='비밀번호 일치'
+              validateFirst={(t) => isSamePassword(password, t)}
+              secureTextEntry={true}
+            />
+          </View>
+        </View>
+
+        <LongButton
+          label={isPending ? '확인 중...' : '휴대폰 본인인증'}
+          onPress={onPressPhoneAuth}
+          disabled={!canSubmit || isPending}
+        />
+      </View>
+
+      <BottomSheetLayout snapPoints={snapPoints} sheetRef={sheetRef}>
+        {/* ===== 서비스 약관 동의 ===== */}
+        <View className='px-5 pt-[30px] pb-[45px]'>
+          <Pressable
+            onPress={toggleAll}
+            className='flex-row items-center mb-[28px] '
+            hitSlop={8}
+          >
+            {isAllChecked ? (
+              <CircleCheckGreenIcon className='mr-[11px]' />
+            ) : (
+              <CircleCheckGrayIcon className='mr-[11px]' />
+            )}
+
+            <Text className='text-b-md font-n-eb text-gray-700 items-center'>
+              서비스 이용 약관 동의(전체)
+            </Text>
+          </Pressable>
+
+          <View className='gap-y-[25px] mb-[25px]'>
+            {TERMS.map(({ id, terms, essential }, idx) => {
+              const checked = checkedSet.has(idx);
+              return (
+                <View
+                  key={idx}
+                  className='flex-row items-center justify-between'
+                >
+                  <Pressable
+                    onPress={() => toggleItem(idx)}
+                    className='flex-row items-center '
+                    hitSlop={8}
+                  >
+                    <Svg
+                      width={18}
+                      height={18}
+                      viewBox='0 0 18 18'
+                      fill='none'
+                      className='mr-[13px]'
+                    >
+                      <Path
+                        d='M16.5 4.5L6.5 15L1.5 9'
+                        stroke={checked ? '#50D88F' : '#C9CDD2'}
+                        strokeWidth={1.5}
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                      />
+                    </Svg>
+                    {essential ? (
+                      <Text
+                        className={`text-b-sm font-n-bd text-gray-400 ${checked ? 'text-gray-700' : 'text-gray-400'}`}
+                      >
+                        [필수]
+                      </Text>
+                    ) : (
+                      <Text
+                        className={`text-b-sm font-n-bd text-gray-400 ${checked ? 'text-gray-700' : 'text-gray-400'}`}
+                      >
+                        [선택]
+                      </Text>
+                    )}
+                    <Text
+                      className={`ml-[2px] font-n-bd text-b-sm ${
+                        !checked && 'text-b-sm text-gray-400'
+                      }`}
+                    >
+                      {terms}
+                    </Text>
+                  </Pressable>
+                  <Pressable
+                    onPress={() => {
+                      setIsTermsModalVisible(true);
+                      setSelectedTerm(id);
+                    }}
+                  >
+                    <RightArrowIcon />
+                  </Pressable>
+                </View>
+              );
+            })}
+          </View>
+          <LongButton
+            label='동의하고 계속하기'
+            onPress={onPressSubmit}
+            disabled={!isEssentialChecked}
+          />
+        </View>
+      </BottomSheetLayout>
 
       <HomeFooterModal
         type={
