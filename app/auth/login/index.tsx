fcbc7e3bcf3b9b2ed84f06ab537e07068065610f
Bangdayeon <dybang00@gmail.com>
2026-01-04 09:51:53 +0900

    refactor: remove tracking (#129)

--- app/auth/login/index.tsx
+++ app/auth/login/index.tsx
@@ -164,7 +164,7 @@ export default function Index({ emergency }: { emergency?: string }) {
   //       sheetRef.current?.close?.();
   //       router.replace('/home');
   //     },
-  //     onError: (err: any) => {
+  //     onError: (err: unknown) => {
   //       console.log('finalizeLogin error:', err);
   //     },
   //   });
