48a79cbdb1fc77655c47cc5928ce66f0adc33486
Bangdayeon <dybang00@gmail.com>
2026-08-05 21:05:07 +0900

    coupang api update

--- app/product/[id]/productDetail.tsx
+++ app/product/[id]/productDetail.tsx
@@ -452,6 +452,7 @@ export default function ProductDetail() {
           )
         }
       />
+      {/* data.coupang */}
       <CoupangTabBar id={id} />
       <PortalHost name='overlay-top' />
       <DefaultModal
